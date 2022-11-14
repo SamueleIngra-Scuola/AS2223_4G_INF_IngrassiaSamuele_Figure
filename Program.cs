03c8dd0fab4be63402e69be19a25158c303f69cd
Samuele Ingrassia <samuele.ingrassia@studenti.ittsrimini.edu.it>
2022-11-14 11:10:32 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2223_4G_INF_IngrassiaSamuele_Figure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_IngrassiaSamuele_Figure
+{
+    internal class Shape
+    {
+    }
+}
