54d48fb489f72737f6a49fc16c563c728f393be5
Samuele Ingrassia <samuele.ingrassia@studenti.ittsrimini.edu.it>
2022-11-28 11:45:44 +0100

    feat: Added TestSquare, TestCircle, TestRectangle

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -13,7 +13,29 @@ namespace UnitTest
         public void TestCircle()
         {
             Circle shape = new Circle(5);
-            Assert.Pass();
+
+            Assert.That(shape.Area, Is.EqualTo(78.539));
+            Assert.That(shape.Perimeter, Is.EqualTo(31.41));
+        }
+
+        [Test]
+        public void TestSquare()
+        {
+            Square shape = new Square(5);
+
+            Assert.That(shape.Area, Is.EqualTo(25));
+            Assert.That(shape.Perimeter, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void TestRectangle()
+        {
+            Rectangle shape = new Rectangle(5, 10);
+
+
+            Assert.That(shape.Area, Is.EqualTo(50));
+            Assert.That(shape.Perimeter, Is.EqualTo(30));
+
         }
     }
 }
